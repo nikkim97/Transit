c159b84ccfcb1ec4d89c558081229819f16711e5
Niharika Mishra <niharikamishr@gmail.com>
2021-11-02 12:01:18 -0400

    more tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -15,3 +15,13 @@ def test_times_post_200():
     parms = {'Time': "08:00"}
     response = requests.post(HOST_URL + "/times", params=parms)
     assert response.status_code == 200
+
+def test_train_post_400():
+    parms = {'Train': "NYC1", 'Times': "[19:00, 18:00]"}
+    response = requests.post(HOST_URL + "/trains", params=parms)
+    assert response.status_code == 400
+
+def test_times_post_400():
+    parms = {'Time': "8:00"}
+    response = requests.post(HOST_URL + "/times", params=parms)
+    assert response.status_code == 400
