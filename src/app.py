5e49500ffbf6d29273ce98f9c43a329268a8d2f1
Niharika Mishra <niharikamishr@gmail.com>
2021-11-02 12:08:52 -0400

    cleaning up return statements

--- src/app.py
+++ src/app.py
@@ -6,62 +6,18 @@ import utilities as ut
 app = Flask(__name__)
 api = Api(app)
 
-class Times(Resource):
-    def post(self):
-        """
-        Returns trains based on given time input
-
-            Parameters:
-                Time (string): Given time value
-
-            Returns:
-                Train (list): Train names associated with next time value
-        """
-        parse = reqparse.RequestParser()
-
-        parse.add_argument('Time', required=True, help="Need to specify train time (Time: 16:00)")
-        args = parse.parse_args()
-        given_time = args['Time']
-
-        if not ut.validate_time_format([given_time]) :
-            return {
-                'message': f"' Please make sure '{given_time}' is in correct HH:MM 24 hr format."
-            }, 400
-
-        mapping_times = {}
-        for train in db.keys():
-            converted = ut.convert_str_to_list(db.fetch(train))
-            for time_val in converted:
-                if time_val in mapping_times:
-                    mapping_times[time_val].append(train)
-                else:
-                    mapping_times[time_val] = [train]
-
-        greater_list = [i for i in sorted(mapping_times.keys()) if i >= given_time]
-        lesser_list = [i for i in sorted(mapping_times.keys()) if i < given_time]
-
-        for time_value in greater_list:
-            if len(mapping_times[time_value])>=2:
-                return f"'{time_value}','{mapping_times[time_value]}'", 200
-
-        for time_value in lesser_list:
-            if len(mapping_times[time_value])>=2:
-                return f"'{time_value}','{mapping_times[time_value]}'", 200
-
-        return f"No instances when multiple trains are in the station at {given_time}", 200
-
 class Trains(Resource):
     def get(self):
         """
         Returns trains schedules
-            Train (Dict): Train names associated with time values
+            Train (Dict): Train names and associated time values
         """
         data = ut.return_dict()
         return data, 200
 
     def post(self):
         """
-        Add new train with times to existing database
+        Add new train name and times to existing database
 
             Parameters:
                 Train (string): Name of train
@@ -92,11 +48,55 @@ class Trains(Resource):
             }, 400
         if not ut.valid_timelist_format(new_times) or not ut.validate_time_format(times_list):
             return {
-                'message': "Please make sure time entry is in [] and in HH:MM 24 hour format. Ex: [08:00, 09:30, 16:00, 18:00]"
+                'message': "Please make sure time entry is in [] and a valid HH:MM 24 hour format. Ex: [08:00, 09:30, 16:00, 18:00]"
             }, 400
         else:
             db.set(new_train, converted_list)
             return ut.return_dict(), 200
+
+class Times(Resource):
+    def post(self):
+        """
+        Returns trains based on given time input
+
+            Parameters:
+                Time (string): Given time value
+
+            Returns:
+                Train (list): Train names associated with next time value
+        """
+        parse = reqparse.RequestParser()
+
+        parse.add_argument('Time', required=True, help="Need to specify train time (Time: 16:00)")
+        args = parse.parse_args()
+        given_time = args['Time']
+
+        if not ut.validate_time_format([given_time]) :
+            return {
+                'message': f"' Please make sure '{given_time}' is a valid HH:MM 24 hr format."
+            }, 400
+
+        mapping_times = {}
+        for train in db.keys():
+            converted = ut.convert_str_to_list(db.fetch(train))
+            for time_val in converted:
+                if time_val in mapping_times:
+                    mapping_times[time_val].append(train)
+                else:
+                    mapping_times[time_val] = [train]
+
+        greater_list = [i for i in sorted(mapping_times.keys()) if i >= given_time]
+        lesser_list = [i for i in sorted(mapping_times.keys()) if i < given_time]
+
+        for time_value in greater_list:
+            if len(mapping_times[time_value])>=2:
+                return f"'{time_value}','{mapping_times[time_value]}'", 200
+
+        for time_value in lesser_list:
+            if len(mapping_times[time_value])>=2:
+                return f"'{time_value}','{mapping_times[time_value]}'", 200
+
+        return f"No instances when multiple trains are in the station at {given_time}", 200
 
 api.add_resource(Trains, '/trains')
 api.add_resource(Times, '/times')
